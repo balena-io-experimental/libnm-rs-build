7e47f642e90b540e8876bf04bd891802a6753255
Zahari Petkov <zahari@balena.io>
2019-03-05 19:00:08 +0200

    Mimetypes for HTML-related extensions
    Signed-off-by: Zahari Petkov <zahari@balena.io>

--- serve.py
+++ serve.py
@@ -10,6 +10,11 @@ Handler = http.server.SimpleHTTPRequestHandler
 
 Handler.extensions_map = {
     '.html': 'text/html',
+    '.png': 'image/png',
+    '.jpg': 'image/jpg',
+    '.svg': 'image/svg+xml',
+    '.css': 'text/css',
+    '.js':  'application/x-javascript',
     '': 'text/plain',
 }
 
